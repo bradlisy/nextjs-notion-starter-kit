ec98b2eb2ae57140bf27f0f5a70d936353d60fd9
bradlisy <36872502+bradlisy@users.noreply.github.com>
2021-10-20 13:00:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,21 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'LI-Jinyang-c2627353557647e18c065ce0a864b1d9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'About Jinyang',
+  domain: 'https://absorbed-comic-d59.notion.site/LI-Jinyang-c2627353557647e18c065ce0a864b1d9',
+  author: 'Jinyang LI',
 
   // open graph metadata (optional)
   description: 'Example site description',
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
